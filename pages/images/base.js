1af40cb9153c167f457cdc1c70ca3c1abf411ebc
anamikaModi25 <anamikamodi1996@gmail.com>
2021-06-09 15:15:03 +0530

    1 file modified, 8 added, 7 deleted

--- pages/images/base.js
+++ pages/images/base.js
@@ -2,13 +2,15 @@ import Image from 'next/image';
 
 const BaseImage = () => {
   return (
-    <div>
+    <div style={{ position: 'relative', width: '300px', height: '500px' }}>
+      Bes
       <Image
-        src="/me.png"
-        alt="Picture of the author"
-        width={500}
-        height={500}
+        alt="Next.js logo"
+        src="https://images.unsplash.com/photo-1623043555134-a67d5a00826e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
+        width={1200}
+        height={400}
       />
+      <Image alt="Mountains" src="/bike.jpg" width={500} height={200} />
     </div>
   );
 };
